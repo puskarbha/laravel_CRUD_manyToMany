210b3a41a80431687232c7d1791da8bd6ccd1fcf
Puskar <puskarbha@gmail.com>
2022-11-08 18:01:01 +0545

    sql backup added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create',function(){
+   $user=User::findOrFail(1);
+  // $role=new Role(['name'=>'administrator']);
+
+   $user->roles()->save(new Role(['name'=>'administrator']));
+});
+
+Route::get('/read',function(){
+   $user=User::findOrFail(1);
+   foreach ($user->roles as $role){
+       echo$role->name;
+   }
+});
+
+Route::get('update',function (){
+   $user=User::findOrFail(1);
+    echo"user found"."<br>";
+   if($user->has('roles')){
+       echo"has roles"."<br>";
+       foreach($user->roles as $role){
+           if($role->name=='administrator'){
+               $role->name='ADMINISTRATOR';
+               $role->save();
+           }
+           else{
+               echo"roles not match"."<br>";
+           }
+       }
+   }
+   else{
+       echo"No Roles";
+   }
 });
