3620aaaa65865d581c775a857728fd83962f954e
Puskar <puskarbha@gmail.com>
2022-11-08 18:55:36 +0545

    attach,detach and sync

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/create',function(){
    $user=User::findOrFail(1);
   // $role=new Role(['name'=>'administrator']);
 
-   $user->roles()->save(new Role(['name'=>'administrator']));
+   $user->roles()->save(new Role(['name'=>'subscriber']));
 });
 
 Route::get('/read',function(){
    $user=User::findOrFail(1);
    foreach ($user->roles as $role){
-       echo$role->name;
+       echo$role->name."<br>";
    }
 });
 
@@ -51,4 +51,32 @@ Route::get('update',function (){
    else{
        echo"No Roles";
    }
+});
+
+Route::get('/delete',function(){
+   $user=User::findOrFail(1);
+
+    foreach ($user->roles as $role){
+        $role->whereId(3)->delete();
+    }
+});
+//attach or add relation user id 1 with role id 6 in pivot column
+Route::get('/attach',function(){
+    $user=User::findOrFail(1);
+    $user->roles()->attach(6);
+
+});
+
+//deattach or remove relation user id 1 with role id 6 in pivot column
+Route::get('/detach',function(){
+    $user=User::findOrFail(1);
+    $user->roles()->detach(6);
+
+});
+
+//add relation if not present,deletes other extra relation
+Route::get('/sync',function(){
+    $user=User::findOrFail(1);
+    $user->roles()->sync([1,3]);
+
 });
